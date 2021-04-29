d70eb27bd965ae230ff2b33015a45ca21b2d5f18
skdbsxir <skdbsxir@gmail.com>
2021-04-29 16:04:25 +0900

    주석 추가, 수정

--- 004. Eigenface & PCA.py
+++ 004. Eigenface & PCA.py
@@ -4,8 +4,9 @@ import sklearn.datasets as datasets
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, adjusted_rand_score
-# silhouette_score : 개별 데이터가 가지는 군집화 지표인 실루엣 계수에 대해 평균 계산. 책 p.174 (10.7.6) 참고.
+# silhouette_score : 개별 데이터가 가지는 군집화 지표인 실루엣 계수에 대해 평균 계산. 10.7.6 참고.
 # adjusted_rand_score : 두 클러스터링 간의 유사성 측정값 계산. 확률에 따라 rand score를 재 조정.
+# rand_score : 군집화 결과와 정답의 유사성 측정.
 from sklearn import metrics
 
 # Data loading
