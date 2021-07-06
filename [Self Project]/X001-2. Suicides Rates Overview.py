295185ab94d47f96ac865016b68c08c4acdfb122
skdbsxir <skdbsxir@naver.com>
2021-07-06 11:50:56 +0900

    OLS에 너무 시간쏟는듯. 대충 공선성 확인만 한 셈 치자.

--- [Self Project]/X001-2. Suicides Rates Overview.py
+++ [Self Project]/X001-2. Suicides Rates Overview.py
@@ -95,6 +95,7 @@ cmap.set_xticklabels(cmap.get_xticklabels(), rotation=0)
 plt.show() # 이전에서 확인했듯이 HDI, gdp_per_capita가 아주높게나옴.
 """
 """
+X_data = KorDF[['HDI_for_year', 'population', 'gdp_per_capita_($)']]
 vif = pd.DataFrame()
 vif['VIF Factor'] = [variance_inflation_factor(X_data.values, i) for i in range(X_data.shape[1])]
 vif['Variables'] = X_data.columns
@@ -102,6 +103,7 @@ print(vif) # HDI (9.9) > gdp (6.36) > population (4.5)
 
 print('\n')
 
+X_data2 = KorDF[['HDI_for_year', 'population']]
 vif2 = pd.DataFrame()
 vif2['VIF Factor'] = [variance_inflation_factor(X_data2.values, i) for i in range(X_data2.shape[1])]
 vif2['Variables'] = X_data2.columns
@@ -128,8 +130,8 @@ print(result3.summary())
  ## 근데 HDI, 100k pop이랑 연관이 조금 있어보임
  ## HDI랑 gdp랑 많이 강한 상관관계가 있음. 
      ### 근데 생각해보면 국민 gdp가 높으니 행복지수가 높은게 당연한거같기도하고
-# target = KorDF[['suicides_no']]
-"""
+target = KorDF[['suicides_no']]
+
 X_data = KorDF[['HDI_for_year', 'population', 'gdp_per_capita_($)']] # R스퀘어 0.242
 scaler = preprocessing.StandardScaler().fit(X_data)
 X_scaled = scaler.transform(X_data)
@@ -138,7 +140,7 @@ myModel4 = sm.OLS(target, X_scaled)
 result4 = myModel4.fit()
 print(result4.summary()) ## 조건수 크다는 문제는 안나왔다 무야호~~~~~~~~
 # 근데 R스퀘어 값이 좀....
-"""
+
 """
 X_data = KorDF[['population']] # R스퀘어 0.583, 
 scaler = preprocessing.StandardScaler().fit(X_data)
@@ -149,3 +151,35 @@ X_scaled = scaler.transform(X_data) # R스퀘어 0.171
 sns.regplot(x = X_scaled, y = target, fit_reg = True)
 """
 # sns.regplot(x=KorDF['gdp_per_capita_($)'], y=KorDF['HDI_for_year'], fit_reg = True)
+
+# 그림만 봐도 아주 올바르게 큰 연관이 있어보인다.
+# 다중공선성 문제.
+# 이래서 target ~ HDI, gdp, population 하면 모형이 안좋게 나오지.
+"""
+Multicollinearity affects the coefficients and p-values, 
+but it does not influence the predictions, precision of the predictions, 
+and the goodness-of-fit statistics. 
+If your primary goal is to make predictions, 
+and you don’t need to understand the role of each independent variable, 
+you don’t need to reduce severe multicollinearity.
+
+https://statisticsbyjim.com/regression/multicollinearity-in-regression-analysis/
+
+하긴 예측이 주 목적이니 공선성 문제를 크게 신경쓰지않아도 되는건가...
+
+위 글 아래부분에서 추가적으로 말해주는 내용
+ - 표준화(standardization)를 해서 데이터를 중앙으로 몰아주면 공선성이 좀 없어질수도 있다고함.
+"""
+
+# 정규화 선형회귀를 해보자 한번.
+X_data = KorDF[['HDI_for_year', 'population', 'gdp_per_capita_($)']]
+# X_data = sm.add_constant(X_data) # 상수항 추가
+
+scaler = preprocessing.StandardScaler().fit(X_data)
+X_scaled = scaler.transform(X_data)
+
+myModel6 = sm.OLS(target, X_scaled)
+result6 = myModel6.fit_regularized(alpha=0.01, L1_wt=0.5, refit = True) # 똑같은데???
+print(result6.summary())
+
+# fit_regularized 했는데 그냥 fit 한거랑 결과가 완전히 똑같이 나온다. 왜지
