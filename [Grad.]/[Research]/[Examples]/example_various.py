a87b4eb400e73bce8507cbf9d89a305ca3d77ef8
CharlieKim <skdbsxir@naver.com>
2022-10-24 19:22:58 +0900

    writing GraphSAGE model

--- [Grad.]/[Research]/[Examples]/example_various.py
+++ [Grad.]/[Research]/[Examples]/example_various.py
@@ -1,5 +1,6 @@
 """
 (Re-build work from official PyG document)
+(GraphSAGE : https://mlabonne.github.io/blog/graphsage/)
 
 TODO: GCNConv, SAGEConv
 
@@ -8,6 +9,8 @@ TODO: GCNConv, SAGEConv
     > 해당 Data class는 안에 x (feature matrix X), edge_index (adj matrix A, COO format), y (true class label), {train/val/test}_mask (mask for node task) 를 가짐. (Planetoid-Cora 기준)
         >> x : (num_nodes, num_node_features) // edge_index : (2, num_edges)
     > Data class의 edge_index를 coo/csc/csr로 변환할 수 있고, 데이터 안에 self loop가 있는지, 고립된 node가 있는지도 확인이 가능.
+- PyG NeighborLoader : https://pytorch-geometric.readthedocs.io/en/latest/modules/loader.html#torch_geometric.loader.NeighborLoader
+    > GraphSAGE에서 소개한 Mini-batch Neighbor Sampling을 수행.
 """
 import os
 
@@ -18,6 +21,9 @@ import torch.optim as optim
 
 import torch_geometric.nn as pyg_nn
 from torch_geometric.datasets import Planetoid
+from torch_geometric.loader import NeighborLoader # for GraphSAGE's neigh sampler
+# import example_various_utils as Utils
+from example_various_utils import GCNUtils
 
 dataset = Planetoid(root=os.getcwd() + '/dataset/cora', name='cora')
 
@@ -55,46 +61,51 @@ class GCN(nn.Module):
         
         output = F.log_softmax(X, dim=1)
 
-        return output
-    
-    def loss(self, pred, label):
-        return F.nll_loss(pred, label)
+        return embedding, output
+
+# GraphSAGE class
+class GraphSAGE(nn.Module):
+    """2-layer"""
+    def __init__(self, data, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.GSage1 = pyg_nn.SAGEConv(in_channels=input_dim, out_channels=hidden_dim, normalize=True, aggr='mean') # Aggregator (mean, max, lstm)
+        self.GSage2 = pyg_nn.SAGEConv(in_channels=hidden_dim, out_channels=output_dim, normalize=True, aggr='mean')
+        self.loader = NeighborLoader(
+            data=data,
+            num_neighbors=[5, 10], # denotes how much neighbors are sampled for each node in each iteration.
+            batch_size = 16, # total batch size
+            input_nodes=data.train_mask # default = None
+        )
+
+    def forward(self, data):
+        X, edge_index = data.x, data.edge_index
+
+        X = self.GSage1(X, edge_index)
+        embedding = X # remember 1st layer's output (embedding after 1st layer-propagation)
+        X = F.relu(X)
+        X = F.dropout(X, training=self.training)
+
+        X = self.GSage2(X, edge_index)
+        
+        output = F.log_softmax(X, dim=1)
+
+        return embedding, output
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model_GCN = GCN(input_dim=dataset.num_node_features, hidden_dim=hidden_dim, output_dim=dataset.num_classes).to(device)
-# print(model_GCN)
 data = dataset[0].to(device)
 
-optimizer = optim.Adam(model_GCN.parameters(), lr=0.01)
+model_GCN = GCN(input_dim=dataset.num_node_features, hidden_dim=hidden_dim, output_dim=dataset.num_classes).to(device)
+model_SAGE = GraphSAGE(data=data, input_dim=dataset.num_node_features, hidden_dim=hidden_dim, output_dim=dataset.num_classes).to(device)
+# print(model_GCN)
+# print(model_SAGE)
 
-def train():
-    model_GCN.train()
 
-    for epoch in range(500):
-        optimizer.zero_grad()
+# embeddings_GCN = Utils.GCN_train(model_GCN, data, 200)
+# test_acc_GCN = Utils.GCN_test(model_GCN, data)
+embeddings_GCN = GCNUtils.GCN_train(model_GCN, data, 200)
+test_acc_GCN = GCNUtils.GCN_test(model_GCN, data)
+print(f'GCN test acc : {test_acc_GCN:.4f}')
 
-        output = model_GCN(data)
-
-        loss = model_GCN.loss(output[data.train_mask], data.y[data.train_mask])
-
-        loss.backward()
-
-        optimizer.step()
-
-        if epoch % 10 == 0:
-            test_acc = test()
-            print(f'[Epoch {epoch}] Train loss : {loss.item():.4f} || Acc : {test_acc:.4f}')
-
-@torch.no_grad()
-def test():
-    model_GCN.eval()
-
-    pred = model_GCN(data).argmax(dim=1)
-    correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
-    
-    acc = int(correct) / int(data.test_mask.sum())
-
-    return acc
-
-train()
+# embeddings_GCN = GCN_train()
+# print(embeddings_GCN.shape)
